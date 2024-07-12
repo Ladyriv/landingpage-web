9f2610408479271f961ee56349a7b54123dec85e
Ladyriv <leidy.riveraf@outlook.com>
2024-07-12 09:55:30 -0500

    adding styles settings

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const menuToggle= document.querySelector(".menu-bars");
-const nav = document.querySelector("nav ul li a");
-
-
-menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("slide");
-});
